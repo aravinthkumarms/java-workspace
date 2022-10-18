0403f9326c0017ca0b6c8b85984a7acfd3ebeab8
Aravinth Kumar <87023106+Aravinth1207@users.noreply.github.com>
2022-10-18 21:36:34 +0530

    Adding prblm description

--- src/com/java/string/isomorphic/Solution.java
+++ src/com/java/string/isomorphic/Solution.java
@@ -3,6 +3,19 @@ package com.java.string.isomorphic;
 import java.util.HashMap;
 import java.util.Map;
 
+// Problem
+//Given two strings s and t, determine if they are isomorphic.
+//
+//        Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+//
+//        All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+//
+//
+//
+//        Example 1:
+//
+//        Input: s = "egg", t = "add"
+//        Output: true
 
 public class Solution {
     public String transformString(String s) {
