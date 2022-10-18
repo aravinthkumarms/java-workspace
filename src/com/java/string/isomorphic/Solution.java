baa2e90d234768a64708022f2f17ab8310f67730
Aravinth Kumar <87023106+aravinthkumarms@users.noreply.github.com>
2022-10-18 21:37:43 +0530

    Isomorphic (#2)
    * isomorphic string solution
    
    * Adding prblm description
    
    Co-authored-by: Aravinth Kumar <87023106+Aravinth1207@users.noreply.github.com>

--- src/com/java/string/isomorphic/Solution.java
+++ src/com/java/string/isomorphic/Solution.java
@@ -1,4 +1,43 @@
 package com.java.string.isomorphic;
 
+import java.util.HashMap;
+import java.util.Map;
+
+// Problem
+//Given two strings s and t, determine if they are isomorphic.
+//
+//        Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+//
+//        All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+//
+//
+//
+//        Example 1:
+//
+//        Input: s = "egg", t = "add"
+//        Output: true
+
 public class Solution {
+    public String transformString(String s) {
+        Map<Character, Integer> indexMapping = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < s.length(); ++i) {
+            char c1 = s.charAt(i);
+
+            if (!indexMapping.containsKey(c1)) {
+                indexMapping.put(c1, i);
+            }
+
+            builder.append(indexMapping.get(c1));
+
+            builder.append(" ");
+        }
+        System.out.println(indexMapping);
+        System.out.println(builder);
+        return builder.toString();
+    }
+    public boolean isIsomorphic(String s, String t) {
+        return transformString(s).equals(transformString(t));
+    }
 }
