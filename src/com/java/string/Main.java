4cc69fa2e2ac04257d9f37e53e5947764929a6f9
Aravinth Kumar <87023106+Aravinth1207@users.noreply.github.com>
2022-10-18 21:51:53 +0530

    Adding another solution

--- src/com/java/string/Main.java
+++ src/com/java/string/Main.java
@@ -1,13 +1,11 @@
 package com.java.string;
 
-import com.java.string.random.Solution;
+import com.java.string.isomorphic.Solution;
 
 class Main {
     public static void main(String[] args) {
-        int[] arr = new int[] { 1, 3, 5, 6 };
         Solution solution = new Solution();
-        System.out.println(solution.searchInsert(arr, 2));
-        System.out.println(arr);
+        solution.isIsomorphic1("add","egg");
     }
 
 }
