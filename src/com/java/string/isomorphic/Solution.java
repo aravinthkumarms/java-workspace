017413b8a6c2e654199541686f846c5f52c3c376
Aravinth Kumar <87023106+Aravinth1207@users.noreply.github.com>
2022-10-18 21:34:54 +0530

    isomorphic string solution

--- src/com/java/string/isomorphic/Solution.java
+++ src/com/java/string/isomorphic/Solution.java
@@ -1,4 +1,30 @@
 package com.java.string.isomorphic;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Solution {
+    public String transformString(String s) {
+        Map<Character, Integer> indexMapping = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < s.length(); ++i) {
+            char c1 = s.charAt(i);
+
+            if (!indexMapping.containsKey(c1)) {
+                indexMapping.put(c1, i);
+            }
+
+            builder.append(indexMapping.get(c1));
+
+            builder.append(" ");
+        }
+        System.out.println(indexMapping);
+        System.out.println(builder);
+        return builder.toString();
+    }
+    public boolean isIsomorphic(String s, String t) {
+        return transformString(s).equals(transformString(t));
+    }
 }
