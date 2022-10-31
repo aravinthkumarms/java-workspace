df0a5a6545b1be4f1b4ab668dcb2e3cb5e1e8d81
Aravinth Kumar <87023106+Aravinth1207@users.noreply.github.com>
2022-10-31 20:33:17 +0530

    Added java files

---
+++ src/com/java/arrays/Main.java
@@ -0,0 +1,9 @@
+package com.java.arrays;
+
+public class Main {
+    public static void main(String[] args){
+        int []  myArray = new int[10];
+      //datatype                      array length
+        int [] myIntArray = {1,2,3,4,5};
+    }
+}
