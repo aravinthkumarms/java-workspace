2f727c22b3f8a74c25b3dc90dd896a4475090af6
Aravinth Kumar <87023106+Aravinth1207@users.noreply.github.com>
2022-11-22 00:23:28 +0530

    Added java files

--- src/com/java/collections/maps/Main.java
+++ src/com/java/collections/maps/Main.java
@@ -12,7 +12,7 @@ public class Main {
             languages.put("Java", "A compiled high level, oo, platform independent language");
             System.out.println("Java added successfully");
         }
-        languages.put("Python", "an interpreted, oo  high level programming language");
+        languages.put("Python", "an interpreted, oo high level programming language");
         languages.put("Algol", "an algorithmic language");
         languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code");
         languages.put("Lisp", "There in lies madness");
@@ -24,7 +24,21 @@ public class Main {
             languages.put("Java","Leaning about java");
         }
         System.out.println("----------------------------------------------------------------");
-
+       // languages.remove("Lisp");
+        if(languages.remove("Algol","an algorithmic language")){
+            System.out.println("Algol removed,");
+        }
+        else{
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+        System.out.println(languages.replace("Lisp"," a functional programming language with imperative features"));
+        // if the old value matches with key , then only replaced with new value
+        if(languages.replace("Lisp"," a functional programming language with imperative features","There in lies madness")){
+           System.out.println("Lisp replaced ");
+       }else{
+           System.out.println("Lisp was not replaced");
+       }
+        System.out.println(languages.replace("Scala","this will not be added"));
         for(String key:languages.keySet()){
             System.out.println(key +" : " +languages.get(key));
         }
